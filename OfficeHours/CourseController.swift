71f505808f7bbe18d863662ced1a94a1753af945
Ismael Alonso <ismaha91@gmail.com>
2016-12-12 15:51:07 -0600

    Populate CourseController

--- OfficeHours/CourseController.swift
+++ OfficeHours/CourseController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 
 class CourseController: UIViewController{
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var meetingTime: UILabel!
+    @IBOutlet weak var lastMeetingDate: UILabel!
+    @IBOutlet weak var instructorName: UILabel!
+    
     
     var course: CourseModel!
     
     
     override func viewDidLoad(){
         navigationItem.title = course.getCode()
+        name.text = course.getName()
+        meetingTime.text = course.getMeetingTime()
+        lastMeetingDate.text = course.getLastMeetingDate()
+        instructorName.text = course.getInstructorName()
     }
 }
